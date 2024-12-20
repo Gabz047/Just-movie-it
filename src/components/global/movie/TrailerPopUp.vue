3b853e24d16adc2ca947f516cfea6fac44b4b56c
Gabz047 <gabe1612mail@gmail.com>
2024-12-20 11:00:59 -0300

    FIX: Fixed

--- src/components/global/movie/TrailerPopUp.vue
+++ src/components/global/movie/TrailerPopUp.vue
@@ -14,7 +14,7 @@ const emits = defineEmits([
 ])
 </script>
 <template>
-    <div v-if="props.play" class="w-screen h-screen bg-[rgba(0,0,0,0.7)] backdrop-blur-sm fixed z-[9999] flex flex-col items-center animating">
+    <div v-if="props.play" class="w-screen h-screen bg-[rgba(0,0,0,0.7)] backdrop-blur-sm fixed z-[99999999] flex flex-col items-center animating">
       <span @click="emits('action')" style="background: linear-gradient(to right, rgba(42,42,42,0.7) 5%, rgba(71,71,71,0.5) 100%)" class="text-white mt-5 backdrop-blur flex h-10 px-6 justify-center items-center gap-4 hover:gap-5 duration-200 hover:scale-[1.03] select-none cursor-pointer rounded-xl">
        <p>Go Back</p>
        <span class="mdi mdi-chevron-right text-xl"></span>
